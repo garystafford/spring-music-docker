5bd30d448f35676bdadfd3e9574163ab55cb394e
Gary A. Stafford <garystafford@rochester.rr.com>
2015-10-02 16:18:06 -0400

    Adding tamplates for NGINX default.conf

---
+++ nginx/default_build_swarm.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+########################################################################
+#
+# title:          NGINX Configuration Template Variable Substitution Script
+# author:         Gary A. Stafford (https://programmaticponderings.com)
+# url:            https://github.com/garystafford/spring-music-docker  
+# description:    Replaces tokens in template
+#
+# to run:         sh default_build_swarm.sh
+#
+########################################################################
+
+# reference: http://www.cyberciti.biz/faq/unix-linux-replace-string-words-in-many-files/
+# http://www.cyberciti.biz/faq/howto-sed-substitute-find-replace-multiple-patterns/
+
+# http://nginx.org/en/docs/http/load_balancing.html
+# ip_hash;
+lb_method_token="#{{ lb_method }}"
+lb_method="" # round-robin by default
+
+app_servers_token="#{{ app_servers }}"
+app_servers="server music_app_1:8080;\r\nserver music_app_2:8080;server music_app_3:8080;"
+
+echo "  ${lb_method_token} = ${lb_method}"
+echo "  ${app_servers_token} = ${app_servers}"
+
+sed -e "s/${lb_method_token}/${lb_method}/g" \
+    -e "s/${app_servers_token}/${app_servers}/g" \
+    < nginx/default-template.conf \
+    > nginx/default.conf
