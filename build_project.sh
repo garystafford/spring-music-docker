4220339e8082ad3d02920536996a3405895548aa
Gary Stafford <garystafford@rochester.rr.com>
2016-07-25 08:26:44 -0400

    Updating project to Docker 1.12

--- build_project.sh
+++ build_project.sh
@@ -4,7 +4,7 @@
 #
 # title:          Build Complete Project
 # author:         Gary A. Stafford (https://programmaticponderings.com)
-# url:            https://github.com/garystafford/sprint-music-docker  
+# url:            https://github.com/garystafford/sprint-music-docker
 # description:    Clone and build complete Spring Music Docker project
 #
 # to run:         sh ./build_project.sh
@@ -13,21 +13,28 @@
 
 # clone project
 git clone -b master \
-  --single-branch https://github.com/garystafford/spring-music-docker.git && 
+  --single-branch https://github.com/garystafford/spring-music-docker.git &&
 cd spring-music-docker
 
 # build VM
-docker-machine create --driver virtualbox springmusic --debug
-
-# create directory to store mongo data on host
-docker-machine ssh springmusic mkdir /opt/mongodb
+docker-machine create --driver virtualbox springmusic
 
 # set new environment
 docker-machine env springmusic && \
 eval "$(docker-machine env springmusic)"
 
+# create directory to store mongo data on host
+docker volume create --name data
+
+# create overlay network
+# docker network create --driver overlay --subnet=10.0.11.0/24 music_overlay_net
+
 # build images and containers
-docker-compose -f docker-compose.yml -p music up -d
+docker-compose -f docker-compose-v2.yml -p music up -d elk && sleep 2 && \
+docker-compose -f docker-compose-v2.yml -p music up -d logspout && sleep 2 && \
+docker-compose -f docker-compose-v2.yml -p music up -d nosqldb && sleep 5 && \
+docker-compose -f docker-compose-v2.yml -p music up -d app01 app02 && sleep 5 && \
+docker-compose -f docker-compose-v2.yml -p music up -d proxy
 
 # configure local DNS resolution for application URL
 #echo "$(docker-machine ip springmusic)   springmusic.com" | sudo tee --append /etc/hosts
@@ -40,3 +47,8 @@ for i in {1..10}
 do
   curl -I --url $(docker-machine ip springmusic)
 done
+
+# useful commands
+# docker rm -f proxy app01 app02 nosqldb elk logspout
+# docker restart elk && sleep 5 && docker restart logspout && sleep 5 && docker restart nosqldb && sleep 10
+# docker restart app01 app02 && sleep 10 && docker restart proxy
